15267535de7ab6201ddfc5b2f08e9f0bd735617a
Hammond <hieutruonghoang01@gmail.com>
2024-02-26 15:48:51 +0700

    Init commit

---
+++ src/Infrastructure/E-commerceSystem.Infrastructure/Implementations/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using E_commerceSystem.Application.Contracts;
+using E_commerceSystem.Application.DTOs;
+using E_commerceSystem.Domain.Entities;
+
+namespace E_commerceSystem.Infrastructure.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<ServiceResponse> AddProductAsync(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse> DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse> UpdateProductAsync(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
