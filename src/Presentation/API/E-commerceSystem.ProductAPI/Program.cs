c8affcd76aad28fd6c69aa522d8ba5ea781fb46e
Hammond <hieutruonghoang01@gmail.com>
2024-02-28 10:47:28 +0700

    Config folder source

--- src/Presentation/API/E-commerceSystem.ProductAPI/Program.cs
+++ src/Presentation/API/E-commerceSystem.ProductAPI/Program.cs
@@ -1,3 +1,7 @@
+using E_commerceSystem.Application.Contracts.ProductAPI;
+using E_commerceSystem.Infrastructure.Data;
+using E_commerceSystem.Infrastructure.Implementations.ProductAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddTransient<IDataAccess, DataAccess>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
